4fc0499b370a4c2ea6d0723c5df10037fdd09a08
Liam <75mcmxc@gmail.com>
2025-04-30 16:12:49 -0700

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,10 @@
 import Header from "../../components/Header";
-// import Budget from "../Budget/Budget";
+import NavBar from "../../components/NavBar";
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* Test */}
-      {/* <Budget /> */}
+      <NavBar />
     </>
   );}
