4dfe6d1df160f7bd4b34fb76474aa8118d10325d
Ben Chan <bchan158@my.bcit.ca>
2025-05-02 12:33:55 -0700

    Add splash screen animations

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -12,13 +12,20 @@ export default function Splash() {
       return;
     }
 
-    const timer = setTimeout(() => navigate("/login"), 2000);
+    const timer = setTimeout(() => navigate("/login"), 3000); //2000
     return () => clearTimeout(timer);
   }, [isSignedIn, navigate]);
 
   return (
     <>
-      <h1>TEST_SPLASH_PAGE</h1>
+      <div className="splash-logo">
+        <img
+          src="src/assets/montro_logo.png"
+          alt="Montro logo"
+          className="bounce-in-top"
+        />
+        <h1 className="bounce-in-fwd">Budgeting, made interesting.</h1>
+      </div>
     </>
   );
 }
