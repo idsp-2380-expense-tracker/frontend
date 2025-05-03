df4da1771ef75d385173f56980d129693d08431b
Ben Chan <bchan158@my.bcit.ca>
2025-05-03 01:50:53 -0700

    Add top spacing

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,9 +5,16 @@ export default function Profile() {
   const displayName = isSignedIn && user ? user.firstName : "Guest";
 
   return (
-    <section className="profile">
-      <img src="src/assets/profile_pic.svg" alt="Profile picture" />
-      <h1>{displayName}</h1>
-    </section>
+    <>
+      <img
+        src="src/assets/left_arrow.png"
+        alt="Back button"
+        className="back-button"
+      />
+      <section className="profile">
+        <img src="src/assets/profile_pic.svg" alt="Profile picture" />
+        <h1>{displayName}</h1>
+      </section>
+    </>
   );
 }
