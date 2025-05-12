c59c1556a92b4bb4d6d4d7bac8d86a9246b5405e
Liam <75mcmxc@gmail.com>
2025-05-12 14:33:03 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,40 +2,19 @@ import reactSWC from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      "react",
-      "react-dom",
-      "@clerk/clerk-react",
-      "@emotion/react",
-      "@emotion/styled",
-      "@mantine/core",
-      "@mantine/hooks",
-      "@mantine/dates",
-      "@mantine/carousel",
-      "@mantine/charts",
-      "@mantine/code-highlight",
-      "@mantine/dropzone",
-      "@mantine/form",
-      "@mantine/modals",
-      "@mantine/notifications",
-      "@mantine/nprogress",
-      "@mantine/spotlight",
-      "@mantine/tiptap",
-      "@mui/material",
-      "@tabler/icons-react",
-      "@tiptap/extension-link",
-      "@tiptap/react",
-      "@tiptap/starter-kit",
-      "axios",
-      "dayjs",
-      "embla-carousel",
-      "embla-carousel-react",
-      "react-datepicker",
-      "react-images-uploading",
-      "react-router-dom",
-      "recharts"
-    ]
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mantine")) return "mantine"
+            if (id.includes("recharts")) return "charts"
+            if (id.includes("dayjs")) return "dayjs"
+            return "vendor"
+          }
+        }
+      }
+    }
   },
   plugins: [
     reactSWC()
